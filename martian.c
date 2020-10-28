df5b769d3af56c1feae6dc56370e38f45c36be0b
Esteban Campos Granados <estebandcg1999@gmail.com>
2020-10-28 05:23:44 -0600

    Simple RMS scheduler with initial threads.

---
+++ martian.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h> 
+
+pthread_cond_t cond_turn = PTHREAD_COND_INITIALIZER; 
+pthread_cond_t cond_exec = PTHREAD_COND_INITIALIZER; 
+pthread_mutex_t lock_exec = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock_turn = PTHREAD_MUTEX_INITIALIZER;
+
+int turn;
+int current_cycle;
+int cycles;
+int finished;
+int multiple;
+int executed;
+
+int size_list;
+
+typedef struct Process {
+    int Id;
+    int Max_Energy;
+    int Current_Energy;
+    int Period;
+    struct Process* Next_Process;
+} node_p;
+
+node_p* add_node(node_p* head, int id, int energy, int period) {
+    if (head == NULL) {
+        head = (node_p*)malloc(sizeof(node_p));
+        head->Id = id;
+        head->Max_Energy = energy;
+        head->Current_Energy = 0;
+        head->Period = period;
+        head->Next_Process = NULL;
+        return head;
+    }
+    else if (period <= head->Period) {
+        node_p* new_head = (node_p*)malloc(sizeof(node_p));
+        new_head->Id = id;
+        new_head->Max_Energy = energy;
+        new_head->Current_Energy = 0;
+        new_head->Period = period;
+        new_head->Next_Process = head;
+        return new_head;
+    }
+    else {
+        node_p* temp = head;
+        while (temp->Next_Process != NULL) {
+            if (period <= temp->Next_Process->Period) {
+                break;
+            }
+            temp = temp->Next_Process;
+        }
+        node_p* new_node = (node_p*)malloc(sizeof(node_p));
+        new_node->Id = id;
+        new_node->Max_Energy = energy;
+        new_node->Current_Energy = 0;
+        new_node->Period = period;
+        new_node->Next_Process = temp->Next_Process;
+        temp->Next_Process = new_node;
+        return head;
+    }
+}
+
+void print_list(node_p* head) {
+    node_p* temp = head;
+    printf("[");
+    while (temp != NULL) {
+        printf("(%d, %d, %d, %d)", 
+            temp->Id, temp->Max_Energy, temp->Current_Energy, temp->Period);
+        temp = temp->Next_Process;
+        if (temp != NULL) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+}
+
+void* exec_thread(void* vargp) {
+    node_p* proc = (node_p*)vargp;
+    while (1) {
+        pthread_mutex_lock(&lock_turn);
+        while (turn != proc->Id) {
+            pthread_cond_wait(&cond_turn, &lock_turn);
+        }
+        if (finished == 1) {
+            turn = -1;
+            printf("T%d finished!\n", proc->Id);
+            pthread_mutex_unlock(&lock_turn);
+            break;
+        }
+        turn = -1;
+        int temp_cycles = cycles;
+        // proc->Current_Energy--;
+        // cycles = 0;
+        while (cycles > 0) {
+            proc->Current_Energy--;
+            cycles--;
+        }
+        printf("T%d executed %d cycles!\n", proc->Id, temp_cycles);
+        cycles = 0;
+        pthread_mutex_unlock(&lock_turn);
+        pthread_mutex_lock(&lock_exec);
+        executed = 1;
+        pthread_cond_signal(&cond_exec);
+        pthread_mutex_unlock(&lock_exec);
+    }
+}
--- planner.c
+++ planner.c
@@ -1,59 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h> 
+#include <pthread.h>
+#include "martian.c"
 
-pthread_mutex_t mutex;
-int turn;
-
-typedef struct Process {
-    int Id;
-    int Time;
-    int Period;
-    struct Process* Next_Process;
-} node_p;
-
-node_p* add_node(node_p* head, int id, int time, int period) {
-    if (head == NULL) {
-        head = (node_p*)malloc(sizeof(node_p));
-        head->Id = id;
-        head->Time = time;
-        head->Period = period;
-        head->Next_Process = NULL;
-    }
-    else {
-        node_p* temp = head;
-        while (temp->Next_Process != NULL) {
-            temp = temp->Next_Process;
-        }
-        node_p* new_node = (node_p*)malloc(sizeof(node_p));
-        new_node->Id = id;
-        new_node->Time = time;
-        new_node->Period = period;
-        new_node->Next_Process = NULL;
-        temp->Next_Process = new_node;
-    }
-    return head;
-}
-
-void print_list(node_p* head) {
-    node_p* temp = head;
-    printf("[");
-    while (temp != NULL) {
-        printf("(%d, %d, %d)", temp->Id, temp->Time, temp->Period);
-        temp = temp->Next_Process;
-        if (temp != NULL) {
-            printf(", ");
-        }
-    }
-    printf("]\n");
-}
-
-void* exec_thread(void* vargp) {
-    printf("FINISHED\n");
-}
-
-int lcm(node_p* head) {
+int lcm(node_p* head) { 
     node_p* temp = head;
     int multiple = temp->Period;
     while (temp->Next_Process != NULL) {
@@ -81,34 +32,138 @@ int lcm(node_p* head) {
     return multiple;
 }
 
+int regen_energy(node_p* head) {
+    node_p* temp = head;
+    while (temp != NULL) {
+        if (current_cycle % temp->Period == 0) {
+            if (temp->Current_Energy != 0) {
+                return -1;
+            }
+            temp->Current_Energy = temp->Max_Energy;
+        }
+        temp = temp->Next_Process;
+    }
+    return 0;
+}
+
+void give_turn(node_p* head, int i) {
+    node_p* temp = head;
+    while (temp != NULL) {
+        if (temp->Current_Energy > 0) {
+            turn = temp->Id;
+            int tmp_cycle;
+            node_p* temp_for_count = head;
+            while (temp_for_count != NULL) {
+                tmp_cycle = current_cycle;
+                while (1) {
+                    tmp_cycle++;
+                    if (tmp_cycle % temp_for_count->Period == 0 
+                    || tmp_cycle - current_cycle == temp->Current_Energy) {
+                        break;
+                    }
+                }
+                if (cycles == 0 || cycles > tmp_cycle - current_cycle) {
+                    cycles = tmp_cycle - current_cycle;
+                }
+                temp_for_count = temp_for_count->Next_Process;
+            }
+            return;
+        }
+        temp = temp->Next_Process;
+    }
+    turn = -1;
+    cycles = 0;
+}
+
+void close_threads() {
+    finished = 1;
+    int n = size_list;
+    for (int i = 0; i < n; i++) { 
+        pthread_mutex_lock(&lock_turn);
+        turn = i;
+        pthread_cond_broadcast(&cond_turn);
+        pthread_mutex_unlock(&lock_turn);
+        usleep(25000);
+    }
+}
+
+void* plan_rm (void* vargp){
+    node_p* head = (node_p*)vargp;
+    while (current_cycle < 19) {
+        if (regen_energy(head) == -1) {
+            printf("Couldn't find a way to schedule the processes!\n");
+            close_threads();
+            return NULL;
+        }
+        give_turn(head, current_cycle);
+        int temp_cycle = cycles;
+        printf("Turn %d: ", current_cycle);
+        if (turn != -1) {
+            pthread_mutex_lock(&lock_turn);
+            pthread_cond_broadcast(&cond_turn);
+            pthread_mutex_unlock(&lock_turn);
+            pthread_mutex_lock(&lock_exec);
+            while (executed == 0) {
+                pthread_cond_wait(&cond_exec, &lock_exec);
+            }
+            executed = 0;
+            pthread_mutex_unlock(&lock_exec);
+            current_cycle += temp_cycle;
+            usleep(25000);
+        }
+        else {
+            printf("skipped!\n");
+            current_cycle++;
+        }
+    }
+    printf("Calendarization ended!\n");
+    close_threads();
+}
+
 int main() {
+
+    /* INIT */
+
     node_p* head = NULL;
-    int times[] = {1, 2, 6};
-    int periods[] = {6, 9, 18};
+    size_list = 3;
+    int times[] = {1, 2, 6}; 
+    int periods[] = {6, 9, 18}; 
 
-    head = add_node(head, 0, times[0], periods[0]);
-    head = add_node(head, 2, times[2], periods[2]);
-    head = add_node(head, 1, times[1], periods[1]);
-    print_list(head);
+    for (int i = 0; i < size_list; i++) { 
+        head = add_node(head, i, times[i], periods[i]);
+    }
 
-    printf("%d\n", lcm(head));
+    /* Planner and threads */
 
-    // pthread_t* threads = (pthread_t*)malloc(sizeof(pthread_t) * 3);
+    pthread_t* planner = (pthread_t*)malloc(sizeof(pthread_t));
+    pthread_t* threads = (pthread_t*)malloc(sizeof(pthread_t) * 3);
 
-    // turn = head->Id;
-    // pthread_mutex_init(&mutex, NULL);
+    /* Global variables */
 
-    // node_p* temp = head;
-    // for (int i = 0; i < 3; i++) {
-    //     pthread_create(threads + i, NULL, exec_thread, (void *) temp);
-    //     temp = temp->Next_Process;
-    // }
+    turn = -1;
+    finished = 0;
+    cycles = 0;
+    multiple = lcm(head);
+    executed = 0;
+    current_cycle = 0;
 
-    // for (int i = 0; i < 3; i++) {
-    //     pthread_join(*(threads + i), NULL);
-    // }
+    /* Start threads */
+    
+    node_p* temp = head;
+    for (int i = 0; i < size_list; i++) { 
+        pthread_create(threads + i, NULL, exec_thread, (void *) temp);
+        temp = temp->Next_Process;
+    }
 
-    // pthread_mutex_destroy(&mutex);
+    pthread_create(planner, NULL, plan_rm, (void *) head);
+
+    /* Join threads */
+
+    pthread_join(*planner, NULL);
+
+    for (int i = 0; i < size_list; i++) {
+        pthread_join(*(threads + i), NULL);
+    }
 
     return 0;
 }
