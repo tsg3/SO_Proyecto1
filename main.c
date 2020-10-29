e6761e35a98766bf56833c0439d92b01b4b9956d
Jose Ortega <jose.ortega@schoolmint.net>
2020-10-29 14:29:57 -0600

    Windows addede, need to be finished

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "movements.c"
 
-void draw_background()
+void draw_background_game()
 {
     for (int i = 0; i < 17; i++)
     {
@@ -18,49 +18,49 @@ void draw_background()
     }
 }
 
-void draw_marcians(ALLEGRO_BITMAP *marcian)
+void draw_marcians()
 {
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < lenght; i++)
     {
-        al_draw_scaled_bitmap(marcian, 0, 0, imageWidth, imageHeight, marcians[i].pos_x, marcians[i].pos_y, imageWidth * 0.5, imageHeight * 0.5, 0);
+        al_draw_scaled_bitmap(marcian_image, 0, 0, imageWidth, imageHeight, marcians[i].pos_x, marcians[i].pos_y, imageWidth * 0.5, imageHeight * 0.5, 0);
     }
 }
 
-void set_cell_maze(MARCIAN *marcian)
+
+void draw_menu()
 {
+    al_draw_bitmap(marcian_image, window_width/2.0f - 32, window_height/2.0f - 20*4 - 72, 0);
+    al_draw_text(font, al_map_rgb(255, 255, 255), window_width/2.0f - 64, window_height/2.0f - 20*4, 0, "STUPID MARCIANS");
+
 }
 
-int main()
+
+void initialize_variables()
 {
-    al_init();
-    al_install_keyboard();
 
-    ALLEGRO_TIMER *timer = al_create_timer(1.0 / 30.0);
-    ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();
-    ALLEGRO_DISPLAY *disp = al_create_display(640, 544);
-    ALLEGRO_FONT *font = al_create_builtin_font();
+    // Window size
+    window_height = 544;
+    window_width = 720;
+    lenght = 0;
 
-    al_register_event_source(queue, al_get_keyboard_event_source());
-    al_register_event_source(queue, al_get_display_event_source(disp));
-    al_register_event_source(queue, al_get_timer_event_source(timer));
-
-    bool redraw = true;
-
+    timer = al_create_timer(1.0 / 30.0);
+    queue = al_create_event_queue();
+    disp = al_create_display(window_width, window_height);
+    font = al_create_builtin_font();
     // Carga sprite
     if (!al_init_image_addon())
     {
         printf("couldn't initialize image addon\n");
-        return 1;
+        exit(1);
     }
-
-    ALLEGRO_BITMAP *marcian_image = al_load_bitmap("assets/marcian2/marcian00.png");
+    marcian_image = al_load_bitmap("assets/marcian2/marcian00.png");
     background = al_load_bitmap("assets/background.png");
     rock = al_load_bitmap("assets/rock.png");
 
     if (!marcian_image || !background)
     {
         printf("couldn't load mysha\n");
-        return 1;
+        exit(1);
     }
 
     float scale = 0.5;
@@ -69,15 +69,130 @@ int main()
     marcians[1] = (MARCIAN){.id = 2, .pos_x = 32, .pos_y = 32, .energy = 4, .period = 9, .direction = 'c'};
     marcians[2] = (MARCIAN){.id = 3, .pos_x = 32, .pos_y = 32, .energy = 4, .period = 9, .direction = 'c'};
 
-    int current = 0;
     steps = 8;
 
     imageWidth = al_get_bitmap_width(marcian_image);
     imageHeight = al_get_bitmap_height(marcian_image);
 
-    al_start_timer(timer);
+    current_window = 'm';
+    running = true;
+    new_energy = 0;
+    new_period = 0;
+}
 
-    while (1)
+void add_new_marcian()
+{
+    if (new_energy != 0 && new_period != 0)
+    {
+        if (new_energy < new_period)
+        {
+            marcians[lenght] = (MARCIAN){.id = 1, .pos_x = 32, .pos_y = 32, .energy = new_energy, .period = new_period, .direction = 'c'};
+            lenght++;
+            printf("New marcian added with energy: %d and period: %d\n", new_energy, new_period);
+            new_period = 0;
+            new_energy = 0;
+        }
+    }
+    else
+    {
+        printf("Energy and period must be greater than zero\n");
+    }
+}
+
+void validate_key()
+{
+    if (event.type == ALLEGRO_EVENT_KEY_DOWN)
+    {
+        switch (event.keyboard.keycode)
+        {
+        case ALLEGRO_KEY_X:
+            if (current_window != 'g')
+                current_window = 'r';
+            else
+                running = false;
+            break;
+
+        case ALLEGRO_KEY_A:
+            if (current_window == 'm')
+                current_window = 'a';
+            else if (current_window == 'a')
+                add_new_marcian();
+            break;
+
+        case ALLEGRO_KEY_S:
+            if (current_window == 'm')
+            {
+                current_window = 'g';
+                game_mode = 'm';
+            }
+            break;
+        case ALLEGRO_KEY_SPACE:
+            if (current_window == 'a')
+            {
+                if (lenght > 0)
+                {
+                    current_window = 'g';
+                    game_mode = 'a';
+                }
+            }
+            else if (current_window == 'g' && game_mode == 'm')
+            {
+                printf("Current mode manual\n");
+                add_new_marcian();
+            }
+            break;
+        case ALLEGRO_KEY_UP:
+            if ((game_mode == 'm' && current_window == 'g') || current_window == 'a')
+            {
+                new_period++;
+            }
+            break;
+        case ALLEGRO_KEY_DOWN:
+            if ((game_mode == 'm' && current_window == 'g') || current_window == 'a')
+            {
+                if (new_period > 0)
+                    new_period--;
+            }
+            break;
+        case ALLEGRO_KEY_RIGHT:
+            if ((game_mode == 'm' && current_window == 'g') || current_window == 'a')
+            {
+                new_energy++;
+            }
+            break;
+        case ALLEGRO_KEY_LEFT:
+            if ((game_mode == 'm' && current_window == 'g') || current_window == 'a')
+            {
+                if (new_energy > 0)
+                    new_energy--;
+            }
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+int main()
+{
+    al_init();
+    al_install_keyboard();
+
+    // Inicializar las variables
+    initialize_variables();
+
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_display_event_source(disp));
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+
+    bool redraw = true;
+
+    al_start_timer(timer);
+    // Marcian manager
+    int current_marcian = 0;
+
+    // Game loop
+    while (running)
     {
 
         al_wait_for_event(queue, &event);
@@ -85,31 +200,48 @@ int main()
         if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
             break;
 
-        make_movement(&marcians[current]);
+        al_clear_to_color(al_map_rgb(0, 0, 0));
 
-        al_clear_to_color(al_map_rgb(185, 75, 36));
+        validate_key();
 
-        draw_background();
-        draw_marcians(marcian_image);
-        if (steps == 0)
+        if (current_window == 'm')
+            draw_menu();
+        else if (current_window == 'g')
         {
-            marcians[current].energy--;
-            marcians[current].direction = 'c';
-            steps = 8;
+
+            draw_background_game();
+
+            if (lenght > 0)
+            {
+                make_movement(&marcians[current_marcian]);
+
+                draw_marcians(marcian_image);
+
+                // Validaciones temporales
+                if (steps == 0)
+                {
+                    marcians[current_marcian].energy--;
+                    marcians[current_marcian].direction = 'c';
+                    steps = 8;
+                }
+                if (marcians[current_marcian].energy == 0)
+                {
+                    marcians[current_marcian].energy = 4;
+                    current_marcian++;
+                    if (&marcians[current_marcian] == NULL)
+                        current_marcian = 0;
+                }
+                if (current_marcian == 3)
+                    current_marcian = 0;
+            }
         }
-        if (marcians[current].energy == 0)
-        {
-            marcians[current].energy = 4;
-            current++;
-        }
-        if (current == 3)
-            current = 0;
 
         al_flip_display();
     }
 
     al_destroy_bitmap(marcian_image);
     al_destroy_bitmap(background);
+    al_destroy_bitmap(rock);
     al_destroy_font(font);
     al_destroy_display(disp);
     al_destroy_timer(timer);
--- standard.h
+++ standard.h
@@ -4,15 +4,33 @@
 #include <allegro5/allegro_image.h>
 #include <unistd.h>
 
+ALLEGRO_TIMER *timer;
+ALLEGRO_EVENT_QUEUE *queue;
+ALLEGRO_DISPLAY *disp;
+ALLEGRO_FONT *font;
 ALLEGRO_BITMAP *background;
 ALLEGRO_BITMAP *rock;
+ALLEGRO_BITMAP *marcian_image;
 ALLEGRO_EVENT event;
 
+int lenght;
 int steps;
 int imageWidth;
 int imageHeight;
 int current_x;
 int current_y;
+int window_height;
+int window_width;
+char current_window;  // m: menu, g: gaming, r: report, a: editing automatic
+char game_mode; // a: automatic, m: manual
+
+// New marcians parameters
+int new_energy;
+int new_period;
+
+
+// Running game variable
+bool running;
 
 typedef struct MARCIAN
 {
@@ -46,4 +64,3 @@ char maze[17][17] = {
     {'o', 'f', 'f', 'f', 'f', 'f', 'f', 'o', 'o', 'f', 'o', 'o', 'o', 'f', 'o', 'o', 'o'},
     {'o', 'o', 'o', 'o', 'o', 'o', 'f', 'f', 'f', 'f', 'o', 'o', 'f', 'f', 'f', 'f', 'm'},
     {'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'}};
-    
